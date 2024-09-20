47e5937c1182180b4345c4d83159c9a11a2ab52c
CarolineLienard <carolineloneshop@gmail.com>
2024-09-20 16:29:41 +0200

    ✨feat : add icon component

--- src/components/GameButton.js
+++ src/components/GameButton.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 import { colors, HeadingM, spacing } from "../styles/theme";
 import { BlackBox } from "../styles/utils";
+import Icon from "./Icon";
 
 const StyledButton = styled.button`
   ${BlackBox}
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   padding: ${spacing.spacing200};
   background-color: ${(props) => props.bgColor || colors.white};
   border: 3px solid ${colors.black};
@@ -12,6 +16,7 @@ const StyledButton = styled.button`
   width: 100%;
   text-align: left;
   transition: border 0.3s, box-shadow 0.3s;
+  position: relative;
   &:hover {
     border: 3px solid ${colors.purple800};
     box-shadow: 0px 10px 0px 0px ${colors.purple800};
@@ -23,11 +28,22 @@ const Label = styled(HeadingM)`
   color: ${(props) => props.textColor || colors.black};
 `;
 
-export default function Button({ label, bgColor, textColor, children }) {
+const IconWrapper = styled.div`
+  position: absolute;
+  right: .8rem;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+export default function Button({ label, bgColor, textColor, icon }) {
   return (
     <StyledButton textColor={textColor} bgColor={bgColor}>
       <Label textColor={textColor}>{label}</Label>
-      {children}
+      {icon && (
+        <IconWrapper>
+          <Icon src={icon} alt="icon" size={80} />
+        </IconWrapper>
+      )}
     </StyledButton>
   );
 }
--- src/pages/MainMenu.js
+++ src/pages/MainMenu.js
@@ -2,12 +2,20 @@ import React from "react";
 import Card from "../components/Card";
 import GameButton from "../components/GameButton";
 import { colors } from "../styles/theme";
+import Icon from "../components/Icon";
+import logo from "../assets/images/logo.svg";
+import players from "../assets/images/player-vs-player.svg"
 
 export default function MainMenu() {
   return (
     <div>
       <Card>
-        <GameButton label="play vs player" bgColor={colors.yellow} />
+        <Icon src={logo} alt="logo" size={52} />
+        <GameButton
+          label="play vs player"
+          bgColor={colors.yellow}
+          icon={players}
+        />
         <GameButton label="game rules" />
       </Card>
       <h1>Menu</h1>
