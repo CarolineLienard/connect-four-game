dd213835293a96f672df0ee5a1b1b2cc0ef4372f
CarolineLienard <carolineloneshop@gmail.com>
2024-09-20 15:19:23 +0200

    ✨ feat : add card container

--- src/pages/MainMenu.js
+++ src/pages/MainMenu.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Card from "../components/Card";
 
 export default function MainMenu() {
   return (
     <div>
+      <Card />
       <h1>Menu</h1>
       <a href="#/game">Game</a>
       <a href="#/rules">Rules</a>
---
+++ src/styles/utils.js
@@ -0,0 +1,7 @@
+import { css } from "styled-components";
+
+export const FlexCSS = css`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
